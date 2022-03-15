9d7716a5207bdd90dccc60c5d7e218cec4285870
Renzel <renzel.david@gmail.com>
2022-03-15 20:06:56 +0000

    refractor fill walk

--- rw_visual.py
+++ rw_visual.py
@@ -5,12 +5,13 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
 
-    # change figure dimensions
-    plt.figure(figsize=(10, 6))
-
     # Make a random walk and plot the points.
     rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # Set the size of the plotting window.
+    plt.figure(figsize=(10, 6))
+
     point_numbers = list(range(50000))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
                 cmap=plt.cm.Blues, edgecolor='none', s=1)
