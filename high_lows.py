b90974f9b99a92805fa0eaf47115cded0a25f0fb
Renzel <renzel.david@gmail.com>
2022-03-18 18:35:34 +0000

    plot low temperatures

--- high_lows.py
+++ high_lows.py
@@ -11,7 +11,7 @@ with open(filename) as f:
     """for index, column_header in enumerate(header_row):
         print(index, column_header)"""
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
@@ -19,12 +19,16 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     # Plot data.
     fig = plt.figure(dpi=128, figsize=(10, 6))
     plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue')
 
     # Format plot.
-    plt.title("Daily high temperatures - 2014", fontsize=24)
+    plt.title("Daily high and low temperatures - 2014", fontsize=24)
     plt.xlabel('', fontsize=16)
     fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
