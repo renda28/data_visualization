c90e7607a88f05ddbac140651149636c8fd90ab0
Renzel <renzel.david@gmail.com>
2022-03-18 12:05:05 +0000

    separate data

--- dice_visual.py
+++ dice_visual.py
@@ -1,29 +1,30 @@
 from die import Die
 import pygal
 
-# Create a D6.
-die = Die()
+# Create two D6 dice.
+die_1 = Die()
+die_2 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-max_result = die.num_sides
-for value in range(1, max_result + 1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visual the results.
 hist = pygal.Bar()
 
-hist.title = "Results of rolling a D6"
-hist.x_labels = [x for x in range(1, max_result + 1)]
+hist.title = "Results of rolling two D6 dice 1000 times."
+hist.x_labels = [x for x in range(2, max_result + 1)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6', frequencies)
-hist.render_to_file('die_visual.svg')
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('dice_visual.svg')
