a62d7db3be7c680c6d4e783cbd6de670c5c76e81
Renzel <renzel.david@gmail.com>
2022-03-19 12:03:23 +0000

    implement error checking

--- high_lows.py
+++ high_lows.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 
 # Get dates and high temperatures from file.
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -13,14 +13,16 @@ with open(filename) as f:
 
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
     # Plot data.
     fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -29,8 +31,9 @@ with open(filename) as f:
     plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
     # Format plot.
-    plt.title("Daily high and low temperatures - 2014", fontsize=24)
-    plt.xlabel('', fontsize=16)
+    plt.title(
+        "Daily high and low temperatures - 2014\nDeath Valley, CA", fontsize=24)
+    plt.xlabel('', fontsize=20)
     fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
