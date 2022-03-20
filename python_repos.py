077c9d66fab1c6df39e398b4131ea2bb066a7af8
Renzel <renzel.david@gmail.com>
2022-03-20 15:58:51 +0000

    add clickable links to graph

--- python_repos.py
+++ python_repos.py
@@ -20,7 +20,9 @@ for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
 
     plot_dict = {
-        'value': repo_dict['stargazers_count'], 'label': repo_dict['description'],
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
     }
 
     plot_dicts.append(plot_dict)
